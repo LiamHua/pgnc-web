44995c37ad2a26625d03f73e65713db47778598b
LiamHua <liamhuazai@gmail.com>
2020-05-05 15:34:52 +0800

    整体logo

---
+++ src/components/Logo/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="logo">
+    <img class="logo-img" src="../../assets/logo.png" alt="logo">
+    <div class="logo-site">
+      <p class="logo-site-name">飞鸽校园</p>
+      <p class="logo-site-address">pgnc.huazai.fun</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  .logo-img {
+    width: 48px;
+    height: 48px;
+  }
+
+  .logo-site {
+    margin-left: 7px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .logo-site-name {
+      font-family: "LiDeBiao-Xing3208533b73821b1b";
+      font-size: 15px;
+    }
+
+    .logo-site-address {
+      font-family: "Malapropism";
+      font-size: 12px;
+    }
+  }
+
+  .logo-site p {
+    line-height: 20px;
+  }
+}
+</style>
