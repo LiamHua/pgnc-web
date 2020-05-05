56b4641b7140c28322251e0e2dcff585df768865
LiamHua <liamhuazai@gmail.com>
2020-05-05 15:34:37 +0800

    主页

---
+++ src/views/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
