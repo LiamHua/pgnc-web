f4595dbe24d82da8b43d0ee76db4aec09ef4c3f8
LiamHua <liamhuazai@gmail.com>
2020-05-07 20:20:14 +0800

    完成轮播图

---
+++ src/components/Carousel/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="carousel">
+    <Carousel autoplay height="440px" loop radius-dot>
+      <CarouselItem v-for="(item, index) in carouseItem" :key="index">
+        <div class="carousel-item">
+          <a :href="item.toLink">
+            <img :src="item.imgLink" :alt="item.comment">
+          </a>
+        </div>
+      </CarouselItem>
+    </Carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      carouseItem: [
+        {
+          id: '',
+          toLink: 'https://www.huazai.fun',
+          imgLink: 'http://img.netbian.com/file/2015/0907/ec2b5261ee74080a832511c13c4de111.jpg',
+          comment: ''
+        },
+        {
+          id: '',
+          toLink: 'https://www.huazai.fun',
+          imgLink: 'http://d.zdqx.com/zkds_170112/001.jpg',
+          comment: ''
+        },
+        {
+          id: '',
+          toLink: 'https://www.huazai.fun',
+          imgLink: 'http://www.08lr.cn/uploads/allimg/170513/1-1F513095438.jpg',
+          comment: ''
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ivu-carousel-item {
+  display: flex;
+  justify-content: center;
+
+  a {
+    display: block;
+  }
+
+  img {
+    height: 440px;
+  }
+}
+</style>
