bde6185ca4441d789f97ee3a1ebbff4dbee82f1b
LiamHua <liamhuazai@gmail.com>
2020-05-07 20:13:18 +0800

    增加分类菜单与轮播图

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,19 +1,45 @@
 <template>
   <div class="home">
     <Header></Header>
+    <div class="content">
+      <div class="top-nav">
+        <ClassMenu></ClassMenu>
+        <Carousel class="tcarousel"></Carousel>
+      </div>
+    </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import ClassMenu from '@/components/ClassMenu'
+import Carousel from '@/components/Carousel'
 
 export default {
   components: {
-    Header
+    Header,
+    Footer,
+    ClassMenu,
+    Carousel
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.content {
+  min-height: calc(100vh - 285px - 60px);
+  width: 60vw;
+  margin: 30px auto;
+}
 
+.top-nav {
+  display: flex;
+  justify-content: flex-start;
+
+  .tcarousel {
+    width: calc(60vw - 165px);
+  }
+}
 </style>
