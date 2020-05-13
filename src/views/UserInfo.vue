e2a140e5d673381ee029def20d6c287cd4342a65
LiamHua <liamhuazai@gmail.com>
2020-05-13 16:15:15 +0800

    用户个人信息页面

---
+++ src/views/UserInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="user-info">
+   <the-header></the-header>
+   <router-view></router-view>
+   <the-footer></the-footer>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader'
+import TheFooter from '@/components/Footer'
+
+export default {
+  components: [
+    TheHeader,
+    TheFooter
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
