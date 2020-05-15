0e034030ea13fefdf010b5c7940c9ef254772960
LiamHua <liamhuazai@gmail.com>
2020-05-15 17:11:47 +0800

    增加个人中心路由

--- src/router/modules/user.js
+++ src/router/modules/user.js
@@ -1,10 +1,46 @@
 const userRoutes = {
   path: '/user',
-  component: () => import('@/views/UserInfo'),
+  // 进入父路由时，默认显示的子路由
+  redirect: '/user/my-info',
+  component: () => import('@/views/UserInfo/index'),
+  meta: {
+    title: '个人中心'
+  },
   children: [
     {
-      path: 'own-info',
-      component: () => import('@/views/UserInfo.vue')
+      path: 'my-info',
+      component: () => import('@/views/UserInfo/my-info'),
+      meta: {
+        title: '个人信息'
+      }
+    },
+    {
+      path: 'my-commodities',
+      component: () => import('@/views/UserInfo/my-commodities'),
+      meta: {
+        title: '我的商品'
+      }
+    },
+    {
+      path: 'shopping-cart',
+      component: () => import('@/views/UserInfo/shopping-cart'),
+      meta: {
+        title: '我的求购'
+      }
+    },
+    {
+      path: 'my-collections',
+      component: () => import('@/views/UserInfo/my-collections'),
+      meta: {
+        title: '我的收藏'
+      }
+    },
+    {
+      path: 'my-authentication',
+      component: () => import('@/views/UserInfo/my-authentication'),
+      meta: {
+        title: '身份认证'
+      }
     }
   ]
 }
