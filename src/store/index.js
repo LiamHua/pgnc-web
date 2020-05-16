5d02a6fee946e5eef15d8cb5a698424c2416d8bd
LiamHua <liamhuazai@gmail.com>
2020-05-17 07:49:01 +0800

    添加登陆与注册组件对显示状态控制

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 控制登陆与注册组件的显示状态
+    showRegister: false
   },
   mutations: {
+    // 切换登陆与注册
+    switchLoginOrRegister (state) {
+      state.showRegister = !state.showRegister
+    }
   },
   actions: {
   },
