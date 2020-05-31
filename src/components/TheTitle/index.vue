34eb3e8432971e1ed877b89d0883de36aa52e8f6
LiamHua <liamhuazai@gmail.com>
2020-05-31 10:43:35 +0800

    feat:增加大字标题

---
+++ src/components/TheTitle/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="the-title">
+    <div class="title-word">
+      <p class="name">飞鸽校园</p>
+      <p class="tip">一起来造作吧</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.the-title {
+  height: 80px;
+  width: 100%;
+  padding: 10px 0;
+  background-color: #fff;
+
+  .title-word {
+    width: 200px;
+    margin: 0 auto;
+    display: flex;
+
+    flex-direction: column;
+    align-items: center;
+
+    .name {
+      line-height: 50px;
+      font-size: 2em;
+    }
+
+    .tip {
+    }
+  }
+}
+</style>
