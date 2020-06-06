2be1747274b94445ae2768894bb7f483bab66ef6
LiamHua <liamhuazai@gmail.com>
2020-06-06 22:05:49 +0800

    增加状态码拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import Vue from 'vue'
 import Config from '@/settings'
 
 const service = axios.create({
@@ -16,13 +17,26 @@ service.interceptors.request.use(
     return config
   },
   error => {
-    console.log(error)
-    Promise.reject(error)
+    return Promise.reject(error)
   }
 )
 
 service.interceptors.response.use(
-
+  response => {
+    //  处理请求成功服务器返回的状态码
+    Vue.prototype.$Message.success(response.data.msg)
+    return response
+  },
+  error => {
+    if (error.response.status === 500) {
+      Vue.prototype.$Message.error('服务器异常，请稍后重试!')
+    } else if (error.response.status === 404) {
+      Vue.prototype.$Message.error('请求页面不存在!!')
+    } else if (error.response.status === 403) {
+      Vue.prototype.$Message.error('权限不足!!!')
+    }
+    return Promise.reject(error)
+  }
 )
 
 export default service
